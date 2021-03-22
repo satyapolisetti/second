0bc0a7d486d0957ad5a32581189b90f0b6306626
satyapolisetti <satyapolisetti>
2021-03-22 15:18:44 +0530

    error

--- first/Program.cs
+++ first/Program.cs
@@ -6,7 +6,7 @@ namespace first
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
         }
     }
 }
